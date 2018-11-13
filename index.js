46535b71b8c899f8dc3cad903067a31c12fff993
mcumm64 <mcumm64@gmail.com>
2018-11-13 22:00:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] ;
 // Add your functions and code here
 
 function destructivelyAppendKitten (array, element) {
-  kittens.push
+  kittens.push(element)
 }
 
 destructivelyPrependKitten
